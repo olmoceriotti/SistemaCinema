d59caa4c53855f2a040d87ae755deccfda799190
Olmo Ceriotti <olmoceriotti@gmail.com>
2023-06-21 21:16:00 +0200

    Migliorato ajax prenotazioni

--- client-web/scripts/proiezione.js
+++ client-web/scripts/proiezione.js
@@ -3,35 +3,58 @@ async function loadPrenotazioni(){
         method: 'GET'
     };
     let response = await fetch('http://localhost:8080/proiezione/', options);
-    let container = document.querySelector(".prenotazione-grid");
+    let container = document.querySelector(".table");
    
     obj = await response.json();
 
-    obj.forEach(async element => {
-        el = await createPrenotazione(element);
-        container.append(el);
+    obj.sort((a, b) => {
+        const dateA = new Date(a.orario[0], a.orario[1] - 1, a.orario[2], a.orario[3], a.orario[4]);
+        const dateB = new Date(b.orario[0], b.orario[1] - 1, b.orario[2], b.orario[3], b.orario[4]);
+        return dateA - dateB;
     });
+
+    const promises = obj.map(async (element) => {
+        let el = await createPrenotazione(element);
+        return el;
+    });
+
+  const elements = await Promise.all(promises);
+    elements.forEach((el) => {
+        container.append(el);
+  });
 }
 
 async function createPrenotazione(obj){
     const prenotazione = document.createElement("tr");
-    prenotazione.classList.add("prenotazione-container");
+    prenotazione.classList.add("table-row");
 
-    let tr = document.createElement("tr");
-    const h6 = document.createElement("h6");
-    h6.textContent = obj.nome;
-    tr.appendChild(h6);
-    prenotazione.appendChild(tr);
+    let div = document.createElement("div");
+    let resFilm = await fetch('http://localhost:8080/film/'  + obj["film id"]);
+    let film = await resFilm.json();
+    div.textContent = film.nome;
+    div.classList.add("film");
+    prenotazione.appendChild(div);
 
-    const img = document.createElement("img");
-    const url = await getPhotoPath(film.externalID);
-    img.src = "https://image.tmdb.org/t/p/original/" + url;
-    filmCard.appendChild(img);
+    const divData = document.createElement("div");
+    divData.textContent = obj.orario[2] + "/" + obj.orario[1] + "/" + obj.orario[0];
+    divData.classList.add("data");
+    prenotazione.appendChild(divData);
 
+    const divOre = document.createElement("div");
+    divOre.textContent = obj.orario[3] + ":"  + obj.orario[4];
+    divOre.classList.add("ora");
+    prenotazione.appendChild(divOre);
+
+    const posti = document.createElement("div");
+    posti.textContent = 150 - parseInt(obj["totale posti occupati"]);
+    posti.classList.add("posti");
+    prenotazione.appendChild(posti);
+
+    const divButton = document.createElement("div");
     const button = document.createElement("button");
-    button.textContent = "More info";
-    button.classList.add("more-info");
-    filmCard.appendChild(button);
+    button.textContent = "Prenota";
+    divButton.appendChild(button);
+    prenotazione.appendChild(divButton);
 
-    return filmCard;
+    return prenotazione;
 }
